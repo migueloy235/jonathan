2b1df0f4f9fbf0cd0341673ec2edcc153170d858
blerome <105025286+blerome@users.noreply.github.com>
2023-05-22 13:01:53 -0500

    Update urls.py

--- urls.py
+++ urls.py
@@ -19,5 +19,7 @@ from django.urls import path
 from miapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index, name="index"),
+    path('index/',views.index, name="index"),
+        path('',views.login, name="login"),
+
 ]
